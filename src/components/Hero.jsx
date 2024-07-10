6c48d97f560f74b4bb1a708b4c73ad407b5813f4
Mujeeb02 <83205006+Mujeeb02@users.noreply.github.com>
2024-07-11 03:03:37 +0530

    frontend completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = ({handleOpenModal}) => {
                 <Link to="/refer-form" onClick={handleOpenModal} className="w-[192px] h-[54px] bg-[#1A73E8] rounded-[8px] ml-[50px] text-[#FFFFFF] text-[20px] leading-[24px] font-[400] flex items-center justify-center">Refer Now</Link>
             </div>
             <div className="w-[814px] h-[725px] overflow-hidden relative">
-                <div className="absolute rotate-180 mt-[220px] left-52 z-[0] h-[87px] w-[157px] ">
+                <div className="absolute rotate-180 mt-[218px] left-52 z-[0] h-[87px] w-[157px] ">
                     <img src="/note1.png" alt="" />
                 </div>  
                 <img src="/hero.png" alt="" className="absolute"/>
